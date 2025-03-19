b51f5a80470735ba90fb9ae91d47a0aa1e8aeaa5
evankelch <evan.c.kelch@gmail.com>
2025-03-19 17:05:32 -0400

    added nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	let {children} = $props();
+</script>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+{@render children()}
+
+<style lang="css">
+		:global(body) {
+				margin: 0;
+		}
+
+	nav {
+			width: 100%;
+			height: 40px;
+			background: darkgreen;
+
+			display: flex;
+			gap: 8px;
+			margin: 0;
+			padding: 0;
+	}
+
+	nav a {
+			margin: 6px 0;
+			padding: 0 12px;
+			align-content: center;
+			border-radius: 4px;
+			text-decoration: none;
+	}
+
+	nav a:hover {
+			background: darkgrey;
+	}
+</style>
